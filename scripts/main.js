e4a64f7226c5ff402c3fcfe21ca9798ed58889bc
Tom Fox <tfox2@hotmail.co.uk>
2019-11-01 01:29:32 +0000

    Insert copy for sections and create fading down carat
    This is it pretty much complete. The text will need some work but
    it's good enough as a placeholder for now. I've also inserted
    gitHub links for each of the projects.

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,36 @@
 const openNav = () => {
   // Close side nav if open
-  if (document.getElementById("sideNav").style.width && document.getElementById("sideNav").style.width !== "0px") {
+  if (document
+      .getElementById("sideNav")
+      .style
+      .width && 
+    document
+      .getElementById("sideNav")
+      .style
+      .width !== "0px") {
     closeNav()
     return
   }
 
   // Close contacts dropdown if open before opening side nav
-  if (document.getElementById("contactsButton").getAttribute("aria-expanded") === "true") {
+  if (document
+    .getElementById("contactsButton")
+    .getAttribute("aria-expanded") === "true") {
     closeContacts()
   }
   
   document
     .getElementById("sideNav")
     .style
-    .width = "12em";
+    .width = "12em"
   document
     .getElementById("main")
     .style
-    .marginLeft = "12em";
+    .marginLeft = "12em"
   const sections = document.querySelectorAll('section')
-  Array.from(sections).forEach((section) => {
+  Array
+    .from(sections)
+    .forEach((section) => {
     section.style.filter = "brightness(80%)"
   })
 }
@@ -28,14 +39,16 @@ const closeNav = () => {
   document
     .getElementById("sideNav")
     .style
-    .width = "0";
+    .width = "0"
   document
     .getElementById("main")
     .style
-    .marginLeft = "0";
+    .marginLeft = "0"
   document.body.style.backgroundColor = "#cbf1f6"
   const sections = document.querySelectorAll('section')
-  Array.from(sections).forEach((section) => {
+  Array
+    .from(sections)
+    .forEach((section) => {
     section.style.filter = "brightness(100%)"
   })
 }
@@ -65,25 +78,90 @@ const scrollHandler = (elementId) => {
 // Change colour of nav elements according to window location
 const colorChanger = () => {
   const sections = document.querySelectorAll('section')
-  Array.from(sections).forEach((section) => {
+  Array
+    .from(sections)
+    .forEach((section) => {
     if (section.offsetTop <= window.scrollY) {
       Array
-      .from(document.querySelectorAll('#topNav button, #topNav a, #topNav a, #sideNav button'))
-      .forEach((element) => {
-        element.style.color = section.getAttribute('data-color')
-      })
+        .from(document.querySelectorAll('#topNav button, #topNav a, #topNav a, #sideNav button'))
+        .forEach((element) => {
+          element.style.color = section.getAttribute('data-color')
+        })
       Array
-      .from(document.querySelectorAll('#sideNav hr'))
-      .forEach((element) => {
-        element.style.backgroundColor = section.getAttribute('data-color')
-      })
+        .from(document.querySelectorAll('#sideNav hr'))
+        .forEach((element) => {
+          element.style.backgroundColor = section.getAttribute('data-color')
+        })
     }
+  })
+}
+
+// Set all section colours according to respective data-color attribute
+const colorSetter = () => {
+  const sections = document.querySelectorAll('section')
+  Array
+    .from(sections)
+    .forEach((section) => {
+    section.style.backgroundColor = section.getAttribute('data-color')
+    section.style.color = section.getAttribute('data-color')
+    Array
+    .from(section.querySelectorAll('#githubLink'))
+    .forEach((element) => {
+      element.style.backgroundColor = section.getAttribute('data-color')
+    })
   })
 }
 
 window.addEventListener('scroll', (event) => {
   colorChanger()
 })
+
+const fade = (element) => {
+  let opacity = 1
+  const timer = setInterval(() => {
+    if (opacity <= 0.01) {
+      clearInterval(timer)
+    }
+    element.style.opacity = opacity
+    element.style.filter = 'alpha(opacity=' + opacity * 100 + ")"
+    opacity -= opacity * 0.05
+  }, 3)
+}
+
+const unfade = (element) => {
+  var opacity = 0.01
+  var timer = setInterval(() => {
+      if (opacity >= 1) {
+          clearInterval(timer)
+      }
+      element.style.opacity = opacity
+      element.style.filter = 'alpha(opacity=' + opacity * 100 + ")"
+      opacity += opacity * 0.05
+  }, 1)
+}
+
+// Temporarily fade down carat to text indicating projects below
+const lookHere = () => {
+  const button = document.querySelectorAll('#scrollHint')[0]
+  fade(button)
+  setTimeout(() => {
+    button.style.fontSize = "1.5em"
+    button.innerHTML = "Check out some of my projects below."  
+    setTimeout(() => {
+      unfade(button)
+      setTimeout(() => {
+        fade(button)
+        setTimeout(() => {
+          button.style.fontSize = "3em"
+          button.innerHTML = "<i class='fas fa-chevron-down'></i>"  
+          setTimeout(() => {
+            unfade(button)
+          }, 80)
+        }, 1000)
+      }, 2000)
+    }, 80)
+  }, 1000)
+}
 
 // Peek side nav element
 window.onload = () => {
@@ -95,8 +173,11 @@ window.onload = () => {
     document.querySelectorAll('#sideNav')[0].style.transition = "0.5s"
     document.querySelectorAll('#main')[0].style.transition = "margin-left 0.5s"
   }, 2000)
-
+  setTimeout(() => {
+    lookHere()
+  }, 3000)
 }
 
+colorSetter()
 colorChanger()
 
